ba20b7c855ae6c12d4a7cd944769e8f6695e532e
Eda Toktogulova <edatoktogulova2003@gmail.com>
2022-02-21 15:45:51 +0600

    added id

--- scsript.js
+++ scsript.js
@@ -19,13 +19,13 @@ const articles = [
       body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, saepe reiciendis asperiores quaerat amet sequi officiis quasi libero ratione eveniet consequuntur incidunt dolores praesentium alias vel sapiente ducimus itaque optio?"
    },
    {
-      id: "article-1",
+      id: "article-2",
       title: "Hello world",
       image: "article",
       body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, saepe reiciendis asperiores quaerat amet sequi officiis quasi libero ratione eveniet consequuntur incidunt dolores praesentium alias vel sapiente ducimus itaque optio?"
    },
    {
-      id: "article-1",
+      id: "article-3",
       title: "Hello world",
       image: "article",
       body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, saepe reiciendis asperiores quaerat amet sequi officiis quasi libero ratione eveniet consequuntur incidunt dolores praesentium alias vel sapiente ducimus itaque optio?"
