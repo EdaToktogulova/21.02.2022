0123ca2291952f455021e10371b7d0a040d2a716
Eda Toktogulova <edatoktogulova2003@gmail.com>
2022-02-21 15:43:26 +0600

    added new element

--- scsript.js
+++ scsript.js
@@ -1,12 +1,33 @@
-document.querySelector('select').addEventListener('input', function() {
-   const value = this.value;
+// document.querySelector('select').addEventListener('input', function() {
+//    const value = this.value;
 
-   document.querySelectorAll('ul').forEach(function(ul) {
-      if (value === ul.id) {
-         ul.className = '';
-      }
-      else {
-         ul.className = 'hidden'
-      }
-   });
-});
+//    document.querySelectorAll('ul').forEach(function(ul) {
+//       if (value === ul.id) {
+//          ul.className = '';
+//       }
+//       else {
+//          ul.className = 'hidden'
+//       }
+//    });
+// });
+
+const articles = [
+   {
+      id: "article-1",
+      title: "Hello world",
+      image: "Cutest_Koala.jpg",
+      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, saepe reiciendis asperiores quaerat amet sequi officiis quasi libero ratione eveniet consequuntur incidunt dolores praesentium alias vel sapiente ducimus itaque optio?"
+   },
+   {
+      id: "article-1",
+      title: "Hello world",
+      image: "article",
+      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, saepe reiciendis asperiores quaerat amet sequi officiis quasi libero ratione eveniet consequuntur incidunt dolores praesentium alias vel sapiente ducimus itaque optio?"
+   },
+   {
+      id: "article-1",
+      title: "Hello world",
+      image: "article",
+      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, saepe reiciendis asperiores quaerat amet sequi officiis quasi libero ratione eveniet consequuntur incidunt dolores praesentium alias vel sapiente ducimus itaque optio?"
+   },
+]
