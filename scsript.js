e8925dc90aa6ab19b88fe15d14942e4af6e452a1
Eda Toktogulova <edatoktogulova2003@gmail.com>
2022-02-21 15:07:33 +0600

    new element

--- scsript.js
+++ scsript.js
@@ -1,5 +1,13 @@
 document.querySelector('select').addEventListener('input', function() {
+   const value = this.value;
+
    document.querySelectorAll('ul').forEach(function(ul) {
-      ul.className = 'hidden';
+      if (value === ul.id) {
+         ul.className = '';
+      }
+      else {
+         ul.className = 'hidden'
+      }
+
    });
-})
+});
