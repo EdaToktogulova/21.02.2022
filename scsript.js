34ba5347f7f25a0ac70a634372bfd3bc6b7977e1
Eda Toktogulova <edatoktogulova2003@gmail.com>
2022-02-25 15:45:18 +0600

    added new element

--- scsript.js
+++ scsript.js
@@ -1,45 +1,8 @@
-// document.querySelector('select').addEventListener('input', function() {
-//    const value = this.value;
 
-//    document.querySelectorAll('ul').forEach(function(ul) {
-//       if (value === ul.id) {
-//          ul.className = '';
-//       }
-//       else {
-//          ul.className = 'hidden'
-//       }
-//    });
-// });
-
-const articles = [
-   {
-      id: "article-1",
-      title: "Hello world",
-      image: "Cutest_Koala.jpg",
-      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, saepe reiciendis asperiores quaerat amet sequi officiis quasi libero ratione eveniet consequuntur incidunt dolores praesentium alias vel sapiente ducimus itaque optio?"
-   },
-   {
-      id: "article-2",
-      title: "Hello world",
-      image: "article",
-      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, saepe reiciendis asperiores quaerat amet sequi officiis quasi libero ratione eveniet consequuntur incidunt dolores praesentium alias vel sapiente ducimus itaque optio?"
-   },
-   {
-      id: "article-3",
-      title: "Hello world",
-      image: "article",
-      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, saepe reiciendis asperiores quaerat amet sequi officiis quasi libero ratione eveniet consequuntur incidunt dolores praesentium alias vel sapiente ducimus itaque optio?"
-   },
-]
-
-const main = document.querySelector('main')
-articles.forEach(article => {
-   const article = document.createElement('article');
-   const h1 = document.createElement('h1');
-   const img = document.createElement('img');
-   const p = document.createElement('p');
-
-   h1.textContent = article.title;
-   img.src
+let details = document.querySelectorAll('details');
+details.forEach((details) => {
+   details.addEventListener('toggle', function(e) {
+      let article = document.querySelector(`${e.target.id}`);
+      article.style.display = "none";
+   });
 });
-
